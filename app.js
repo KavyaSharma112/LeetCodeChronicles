605ef11156d46c7942a25d4fae69a63b1d2c80bd
KavyaSharma112 <153850499+KavyaSharma112@users.noreply.github.com>
2025-06-26 23:55:45 +0530

    initial commit
    index.html, app.js added

---
+++ app.js
@@ -0,0 +1,8 @@
+const welcomeMessage = React.createElement(
+    "h1",
+    {},
+    "Welcome to LeetCode Chronicles!"
+)
+
+const root = ReactDOM.createRoot(document.getElementById("intro"));
+root.render(welcomeMessage);
