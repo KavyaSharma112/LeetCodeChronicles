c2d3eb8a130fab06ae7912f7b323de71ff7fbdb8
KavyaSharma112 <153850499+KavyaSharma112@users.noreply.github.com>
2025-07-06 02:06:54 +0530

    Node Modules added

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-const welcomeMessage = React.createElement(
-    "h1",
-    {},
-    "Welcome to LeetCode Chronicles!"
-)
+import ReactDOM from "react-dom/client";
+import React from "react";
+
+const heading = <h1>This is LeetCode Chronicles !</h1>
 
 const root = ReactDOM.createRoot(document.getElementById("intro"));
-root.render(welcomeMessage);
+root.render(heading);
