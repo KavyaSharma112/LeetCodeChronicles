54b34db2cb4eabe2845dbbfa04eb270b7c0963a9
KavyaSharma112 <153850499+KavyaSharma112@users.noreply.github.com>
2025-08-18 14:53:48 +0530

    Assets added

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,22 +1,22 @@
 import "./Hero.css";
-import logo from "../../assets/icebear.png"; // adjust path as per your structure
+import harry from "../../assets/harryPotter.png"; // adjust path as per your structure
 
 export default function Hero() {
   return (
     <section className="hero">
-      {/* Left: Floating logo */}
+      {/* Left: Text */}
       <div className="hero-left">
-        <img src={logo} alt="Logo" className="floating-logo" />
-      </div>
-
-      {/* Right: Text */}
-      <div className="hero-right">
         <h1>
           <i>Solutions</i> are easy. <span>Understanding</span> is rare.
         </h1>
         <p>
           And nothing matches the satisfaction of truly seeing the algorithm in your mind. ✨
         </p>
+      </div>
+
+      {/* Right: Floating logo */}
+      <div className="hero-right">
+        <img src={harry} alt="Logo" className="floating-logo" />
       </div>
     </section>
   );
